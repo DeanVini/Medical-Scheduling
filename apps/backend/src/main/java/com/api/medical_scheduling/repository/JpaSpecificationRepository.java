a14f58285113b3a8c0bebf22b9b7790bc1e349c2
DeanVini <deanvinicius2003@gmail.com>
2025-05-29 20:35:11 -0300

    feat: utility for copying non-null properties
    Introduce `CustomBeanUtils` with a method to copy non-null properties from a source object to a target object. This ensures that null values in the source do not overwrite existing values in the target.

--- apps/backend/src/main/java/com/api/medical_scheduling/repository/JpaSpecificationRepository.java
+++ apps/backend/src/main/java/com/api/medical_scheduling/repository/JpaSpecificationRepository.java
@@ -1,4 +1,4 @@
-package com.api.medical_scheduling.interfaces;
+package com.api.medical_scheduling.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
